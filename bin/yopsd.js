68f7a83e913a668e2f859550580b220a95ffdaaa
jcsibon <jcsibon@gmail.com>
2016-04-15 11:56:58 +0200

    first commit

--- bin/yopsd.js
+++ bin/yopsd.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 var PSD = require('psd');
+var fs = require('fs');
+var chalk = require('chalk');
 
 var file = process.argv[2] || './examples/images/example.psd';
+var path = file.replace(/\.psd$/, '');
 var start = new Date();
-var fileString = '<div class="container">\n';
+
 
 
 
 PSD.open(file).then(function (psd) {
+  var fileString = '<div class="container">\n';
+
   psd.tree().descendants().forEach(function (node) {
     if (node.isGroup()) return true;
     console.log(node.name);
@@ -18,6 +23,18 @@ PSD.open(file).then(function (psd) {
   });
 
   fileString += '</div>';
+
+  fs.writeFile(path + '/index.php', fileString, function(err) {
+    if (err) {
+      console.log(chalk.red.bold("Error while saving %s"), fileHtml);
+      return cb(err);
+    }
+
+    console.log(chalk.gray("HTML saved to %s"), fileHtml);
+    filesProcessed.push(fileHtml);
+    cb(null, fileHtml);
+  });
+
 }).then(function () {
   console.log("Finished in " + ((new Date()) - start) + "ms");
 }).catch(function (err) {
